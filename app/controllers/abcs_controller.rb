20924abf9d901278da24b4a132510c4ebfc899eb
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-11-12 00:07:45 +0300

    change name csv-file

--- app/controllers/abcs_controller.rb
+++ app/controllers/abcs_controller.rb
@@ -6,12 +6,12 @@ class AbcsController < ApplicationController
   def show; end
 
   def import
-    FileUtils.rm_rf(Dir.glob('public/provider/*'))
+    FileUtils.rm_rf(Dir.glob('public/stock.csv'))
     uploaded_io = params[:file]
-    File.open(Rails.root.join('public', 'provider', uploaded_io.original_filename), 'wb') do |file|
+    File.open(Rails.root.join('public', uploaded_io.original_filename), 'wb') do |file|
       file.write(uploaded_io.read)
     end
-    path_file = Rails.root.join('public', 'provider', uploaded_io.original_filename).to_s
+    path_file = Rails.root.join('public', uploaded_io.original_filename).to_s
     extend_file = uploaded_io.original_filename.to_s
     AbcImportJob.perform_later(path_file, extend_file)
     flash[:notice] = 'Задача импорта Товаров запущена'
