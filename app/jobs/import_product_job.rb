4540d76f44f0991c6471c8dc12e7eef4bddc2c1a
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-11-11 23:38:05 +0300

    comment send mail

--- app/jobs/import_product_job.rb
+++ app/jobs/import_product_job.rb
@@ -3,11 +3,11 @@ class ImportProductJob < ApplicationJob
 
   def perform
     Services::ImportProduct.call
-    data_email = {
-      email: 'mm062@mail.ru',
-      subject: 'Оповещение: Закончено обновление Товаров SanTitan',
-      body: 'Закончено обновление Товаров SanTitan'
-    }
-    NotificationMailer.notify(data_email).deliver_later
+    # data_email = {
+    #   email: 'mm062@mail.ru',
+    #   subject: 'Оповещение: Закончено обновление Товаров SanTitan',
+    #   body: 'Закончено обновление Товаров SanTitan'
+    # }
+    # NotificationMailer.notify(data_email).deliver_later
   end
 end
