eef3df963206c0b28236208e4b9d3da0375edb86
Yegor Tikhanin <yegor.tikhanin@gmail.com>
2021-11-30 18:47:36 +0300

    change export csv view

--- app/services/export_csv.rb
+++ app/services/export_csv.rb
@@ -12,17 +12,26 @@ class Services::ExportCsv
                  .order(:id)
 
     CSV.open("#{Rails.root}/public/export_insales.csv", "wb") do |writer|
-      headers = [ 'ID варианта товара', 'Название товара', 'Артикул', 'Цена продажи', 'Остаток' ]
+      headers = ['fid', 'Артикул', 'Название товара', 'Полное описание', 'Производитель', 'Цена продажи', 'Цена старая',
+                 'Остаток', 'Изображения', 'Url', 'Подкатегория 1', 'ID товара в InSales', 'ID варианта товара в InSales']
 
       writer << headers
       products.each do |pr|
-        productid_var_insales = pr.insales_var_id
-        title = pr.title
+        fid = pr.id
         sku = pr.sku
+        title = pr.title
+        desc = pr.desc
+        distributor = pr.distributor
         price = pr.price
+        oldprice = pr.oldprice
         quantity = pr.quantity
+        image = pr.image
+        url = pr.url
+        cat = pr.cat
+        insales_id = pr.insales_id
+        insales_var_id = pr.insales_var_id
 
-        writer << [productid_var_insales, title, sku, price, quantity ]
+        writer << [fid, sku, title, desc, distributor, price, oldprice, quantity, image, url, cat, insales_id, insales_var_id]
       end
     end #CSV.open
   end
